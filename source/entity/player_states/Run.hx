09c41fe4e17e8484517f91f087f99c86af63d64b
aiglebleu <aiglebleu@openmailbox.org>
2017-04-09 13:48:03 -0400

    Run baby run

---
+++ source/entity/player_states/Run.hx
@@ -0,0 +1,18 @@
+package entity.player_states;
+
+import addons.FlxFSM;
+
+class Run extends FlxFSMState<Player>
+{
+    override public function enter(owner:Player, fsm:FlxFSM<Player>):Void 
+    {
+        owner.animation.play("run");
+        owner.speedFactor = 1.2;
+    }
+    
+    override public function update(elapsed:Float, owner:Player, fsm:FlxFSM<Player>):Void 
+    {
+        if (owner.speedFactor < 1.5)
+            owner.speedFactor += 0.01;
+    }
+}
