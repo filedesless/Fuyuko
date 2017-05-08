1409a77e75d4376ad2752d2c82dba1fc4d7902e9
KouramaProject <kp.gpelletier@gmail.com>
2017-05-08 08:52:01 -0400

    Suraimu Vert Bestiaire

--- source/scene/BestiaryState.hx
+++ source/scene/BestiaryState.hx
@@ -25,6 +25,11 @@ class BestiaryState extends FlxState {
     var _canvasShokuka:FlxSprite;
     var _entityShokuka:FlxSprite;
 
+    var _groupSuraimu:FlxSpriteGroup;
+    var _canvasSuraimu:FlxSprite;
+    var _entitySuraimu:FlxSprite;
+    var _btnGroupSuraimu:FlxSpriteGroup;
+
     var _groups:FlxSpriteGroup = new FlxSpriteGroup();
     var _btnGroups:FlxSpriteGroup = new FlxSpriteGroup();
     var _cnt:Int = 0;
@@ -33,6 +38,7 @@ class BestiaryState extends FlxState {
     var _btnFuyuko:FelixMagicButton;
     var _btnEkunaa:FelixMagicButton;
     var _btnShokuka:FelixMagicButton;
+    var _btnSuraimu:FelixMagicButton;
     var _load:Loading;
 
     override public function create():Void {
@@ -48,6 +54,7 @@ class BestiaryState extends FlxState {
             buildFuyuko();
             buildEkunaa();
             buildShokuka();
+            buildSuraimu();
             buildUI();
             _cnt++;
         } else _load.kill();
@@ -56,7 +63,7 @@ class BestiaryState extends FlxState {
     }
 
     function buildUI():Void {
-        if (_cnt == 47) {
+        if (_cnt == 67) {
             _groups.add(_btnGroups);
             _btnRetour = new FelixMagicButton(
                 25, FlxG.height - 100, this, "Retour", clickQuitter
@@ -66,26 +73,32 @@ class BestiaryState extends FlxState {
             add(_btnGroups);
         }
         
-        if (_cnt == 49)
+        if (_cnt == 69)
         _btnFuyuko = new FelixMagicButton(
             -50, 150, this, "Fuyuko", showFuyuko
         );
 
-        if (_cnt == 51)
+        if (_cnt == 71)
         _btnEkunaa = new FelixMagicButton(
-            -50, 250, this, "Ekunaa", showEkunaa
+            -50, 225, this, "Ekunaa", showEkunaa
         );
 
-        if (_cnt == 53)
+        if (_cnt == 73)
         _btnShokuka = new FelixMagicButton(
-            -50, 350, this, "Shokuka", showShokuka
+            -50, 300, this, "Shokuka", showShokuka
         );
 
-        if (_cnt == 55) {
+        if (_cnt == 75)
+        _btnShokuka = new FelixMagicButton(
+            -50, 375, this, "Suraimu", showSuraimu
+        );
+
+        if (_cnt == 75) {
             add(_btnRetour);
             add(_btnFuyuko);
             add(_btnEkunaa);
             add(_btnShokuka);
+            add(_btnSuraimu);
             showFuyuko();
         }
     }
@@ -282,6 +295,76 @@ class BestiaryState extends FlxState {
             case "jump": _entityFuyuko.animation.play("fall");
             case "fall": _entityFuyuko.animation.play("reco");
         }
+    }
+
+    function buildSuraimu():Void {
+        if (_cnt == 50) {
+            _canvasSuraimu = new FlxSprite(0, 0, AssetPaths.beast_suraimu_vert__png);
+            _entitySuraimu = new FlxSprite();
+            _entitySuraimu.loadGraphic(AssetPaths.charsheet_suraimu_vert_charsheet__png, true, 256, 192);
+            _entitySuraimu.scale.set(0.25, 0.25);
+            _entitySuraimu.updateHitbox();
+            _entitySuraimu.setPosition(80, 20);
+        }
+        
+        if (_cnt == 55) {
+            _entitySuraimu.animation.add("idle", [for (i in 7...14) i], 8, true);
+            _entitySuraimu.animation.add("walk", [for (i in 28...35) i], 12, true);
+            _entitySuraimu.animation.add("jump", [for (i in 22...27) i], 16, false);
+            _entitySuraimu.animation.add("bubble", [for (i in 0...7) i], 24, true);
+            _entitySuraimu.animation.add("splash", [15,16,17,18,19,35,36,37,38,39,40,41], 8, false);
+            _entitySuraimu.animation.play("idle");
+        }
+        
+        if (_cnt == 60) {
+            _btnGroupSuraimu = new FlxSpriteGroup();
+            _btnGroupSuraimu.add(new FelixMagicButton(
+                FlxG.width, 0, this, "Idle", function() { _entitySuraimu.animation.play("idle"); }
+            ));
+            _btnGroupSuraimu.add(new FelixMagicButton(
+                FlxG.width, 75, this, "Marche", function() { _entitySuraimu.animation.play("walk"); }
+            ));
+            _btnGroupSuraimu.add(new FelixMagicButton(
+                FlxG.width, 150, this, "Saut", function() { _entitySuraimu.animation.play("Jump"); }
+            ));
+            _btnGroupSuraimu.add(new FelixMagicButton(
+                FlxG.width, 225, this, "Bubble", function() { _entitySuraimu.animation.play("Bubble"); }
+            ));
+            _btnGroupSuraimu.add(new FelixMagicButton(
+                FlxG.width, 300, this, "Splash", function() { _entitySuraimu.animation.play("Splash"); }
+            ));
+            _btnGroupSuraimu.screenCenter(FlxAxes.Y);
+        }
+        
+        if (_cnt == 65) {
+            _groupSuraimu = new FlxSpriteGroup();
+            _groupSuraimu.add(_canvasSuraimu);
+            _groupSuraimu.add(_entitySuraimu);
+            _groupSuraimu.screenCenter(FlxAxes.X);
+            _groupSuraimu.y = 40;
+
+            _btnGroups.add(_btnGroupSuraimu);
+            _groups.add(_groupSuraimu);
+        }
+    }
+
+    function showSuraimu():Void {
+        _btnGroupFuyuko.forEachOfType(FelixMagicButton, function(btn:FelixMagicButton):Void {
+            FlxTween.tween(btn, { x: FlxG.width }, 0.5);
+        });
+        _btnGroupSuraimu.forEachOfType(FelixMagicButton, function(btn:FelixMagicButton):Void {
+            FlxTween.tween(btn, { x: FlxG.width }, 0.5);
+        });
+
+        remove(_groupFuyuko);
+        remove(_groupEkunaa);
+        remove(_groupShokuka);
+        add(_groupSuraimu);
+
+        _btnFuyuko.enable();
+        _btnEkunaa.enable();
+        _btnShokuka.enable();
+        _btnSuraimu.disable();
     }
 
     function clickQuitter():Void {
